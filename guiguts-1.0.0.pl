b8a95ccda23cd8836c91810227e3e8e2f45477a3
vlsimpson <vlsimpson@6abef783-aa3e-0410-acc4-8c2641982bcc>
2008-11-21 22:37:52 +0000

    Added File/Search/Help menu stubs.

--- guiguts-1.0.0.pl
+++ guiguts-1.0.0.pl
@@ -17,7 +17,6 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-
 use warnings;
 use strict;
 
@@ -26,14 +25,18 @@ use lib "$FindBin::Bin/lib";
 use Data::Dump;
 
 use Tkx;
-$Tkx::TRACE = 1;
 
+$Tkx::TRACE = 0;
+
+our $VERSION = "1.0.0";
+
+(my $progname = $0) =~ s,.*[\\/],,;
+my $IS_AQUA = Tkx::tk_windowingsystem() eq "aqua";
 
 
 # Main window container
 my ($mw, $tw);
 $mw = Tkx::widget->new(".");
-
 $mw->configure(-menu => mk_menu($mw));
 
 # Our text widget
@@ -43,27 +46,113 @@ $tw = $mw->new_text(
 $tw->g_pack;
 $tw->g_focus;
 
-$tw->insert("1.0", "If you can read this it worked.");
+$tw->insert("end", "If you can read this it worked");
+$tw->insert("end", " but I don't do much at the moment.");
 
 Tkx::MainLoop();
 exit;
 
-## Functions
+### Functions
 
-# GUI building routines
+## GUI building routines
+
+# Main menu
+# FIXME: Abstract this out to some kind of hash; e.g.,
+# %menu = ( -label => $label, -underline => $underline, -menu => $menu )
+
 sub mk_menu {
-    my $mw = shift(@_);
+    my $mw = shift;
     my $menu = $mw->new_menu;
 
-    my $file = $menu->new_menu( -tearoff => 0,
+# File menu item    
+    my $file = $menu->new_menu(
+        -tearoff => 0,
     );
     $menu->add_cascade(
         -label => "File",
         -underline => 0,
         -menu => $file,
     );
+    $file->add_command(
+        -label => "New",
+        -underline => 0,
+        -accelerator => "Ctrl+N",
+        -command => \&new,
+    );
+    $mw->g_bind("<Control-n>", \&new);
+    $file->add_command(
+        -label => "Open",
+        -underline => 0,
+        -accelerator => "Ctrl+O",
+        -command => \&open_file,
+    );
+    $mw->g_bind("<Control-o>", \&open_file);
+    $file->add_command(
+        -label   => "Exit",
+        -underline => 1,
+        -command => [\&Tkx::destroy, $mw],
+    ) unless $IS_AQUA;
+
+    # Search menu item
+    my $search = $menu->new_menu(
+        -tearoff => 1,
+    );
+    $menu->add_cascade(
+        -label => "Search",
+        -underline => 0,
+        -menu => $search,
+    );
+    $search->add_command(
+        -label => "Search",
+        -underline => 0,
+        -accelerator => "Ctrl+s",
+        -command => \&search_box,
+    );
+
+    # Help menu item
+    my $help = $menu->new_menu(
+        -name => "help",
+        -tearoff => 0,
+    );
+    $menu->add_cascade(
+        -label => "Help",
+        -underline => 0,
+        -menu => $help,
+    );
+    $help->add_command(
+        -label => "\u$progname Manual",
+        -command => \&show_manual,
+    );
+
+    my $about_menu = $help;
+    if ($IS_AQUA) {
+        # On Mac OS we want about box to appear in the application
+        # menu.  Anything added to a menu with the name "apple" will
+        # appear in this menu.
+        $about_menu = $menu->new_menu(
+            -name => "apple",
+        );
+        $menu->add_cascade(
+            -menu => $about_menu,
+        );
+    }
+    $about_menu->add_command(
+        -label => "About \u$progname",
+        -command => \&about,
+    );
 
     return $menu;
+}
+
+
+sub about {
+    Tkx::tk___messageBox(
+        -parent => $mw,
+        -title => "About \u$progname",
+        -type => "ok",
+        -icon => "info",
+        -message => "$progname v$VERSION\nV. L. Simpson 2008.  All rights reserved.",
+    );
 }
 
 
