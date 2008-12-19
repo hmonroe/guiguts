99ab154ff802a6aabe44b33cda0131590a41d7ac
vlsimpson <vlsimpson@6abef783-aa3e-0410-acc4-8c2641982bcc>
2008-12-19 19:18:46 +0000

    Preliminary GutThing-1.0.0.pl draft.

---
+++ GutThing-1.0.0.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+
+# $Id$
+
+# GuiGuts text editor
+
+#Copyright (C) 2008 V. L. Simpson <vlsimpson@gmail.com>
+
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+require 5.008;
+use strict;
+use warnings;
+use FindBin;
+use lib $FindBin::Bin . "/lib";
+use Data::Dumper;
+
+use Cwd;
+use Encode;
+use File::Spec::Functions qw/:ALL/;
+use File::Basename;
+use File::Temp qw/tempfile/;
+use HTML::TokeParser;
+use IPC::Open2;
+use LWP::UserAgent;
+use charnames();
+
+use Tk;
+use Tk::widgets qw/Balloon
+    BrowseEntry
+    Checkbutton
+    Dialog
+    DialogBox
+    DropSite
+    Font
+    JPEG
+    LabFrame
+    Listbox
+    PNG
+    Pane
+    Photo
+    ProgressBar
+    Radiobutton
+    TextEdit/;
+
+# Custom Guigut modules
+use LineNumberText;
+use TextUnicode;
+
