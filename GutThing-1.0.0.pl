638f38c7adb4ce9d8b804416581e0aee8e0f64b3
vlsimpson <vlsimpson@6abef783-aa3e-0410-acc4-8c2641982bcc>
2008-12-26 05:23:14 +0000

    Fleshing out menus.

--- GutThing-1.0.0.pl
+++ GutThing-1.0.0.pl
@@ -37,130 +37,183 @@ use charnames();
 
 use Tk;
 use Tk::widgets qw/Balloon
-    BrowseEntry
-    Checkbutton
-    Dialog
-    DialogBox
-    DropSite
-    Font
-    JPEG
-    LabFrame
-    Listbox
-    PNG
-    Pane
-    Photo
-    ProgressBar
-    Radiobutton
-    TextEdit/;
+  BrowseEntry
+  Checkbutton
+  Dialog
+  DialogBox
+  DropSite
+  Font
+  JPEG
+  LabFrame
+  Listbox
+  PNG
+  Pane
+  Photo
+  ProgressBar
+  Radiobutton
+  TextEdit/;
 
 # Custom Guigut modules
 use LineNumberText;
 use TextUnicode;
 
-
 # ignore any watchdog timer alarms. Subroutines that take a long time to complete can trip it
 $SIG{ALRM} = 'IGNORE';
-$SIG{INT} = sub { myexit() }; # FIXME: Can this be \&myexit;?
+$SIG{INT} = sub { myexit() };    # FIXME: Can this be \&myexit;?
 
-my $VERSION = "1.0.0";
-my $gg_dir = "$FindBin::Bin"; # Get the GuiGuts directory.
+my $VERSION      = "1.0.0";
+my $gg_dir       = "$FindBin::Bin";            # Get the GuiGuts directory.
 my $window_title = "GutThing - " . $VERSION;
-
 
 my %globals;    #All local global variables contained in one hash.
 
 my $mw = tkinit( -title => $window_title );
 $mw->minsize( 440, 90 );
 
-my $text_frame = $mw->Frame->pack( -anchor => 'nw',
-    -expand => 'yes', 
-    -fill => 'both' ) ;          # autosizing
+my $text_frame = $mw->Frame->pack(
+    -anchor => 'nw',
+    -expand => 'yes',
+    -fill   => 'both'
+);              # autosizing
 
 my $textwindow = $text_frame->LineNumberText(
-    -widget => 'TextUnicode',
-    -exportselection => 'true',     # 'sel' tag is associated with selections
+    -widget          => 'TextUnicode',
+    -exportselection => 'true',        # 'sel' tag is associated with selections
     -background      => 'white',
     -relief          => 'sunken',
-#    -font      => $global{font},
-    -wrap      => 'none',
-#    -curlinebg => $activecolor,
-)->pack(
+
+    #    -font      => $global{font},
+    -wrap => 'none',
+
+    #    -curlinebg => $activecolor,
+  )->pack(
     -side   => 'bottom',
     -anchor => 'nw',
     -expand => 'yes',
     -fill   => 'both'
-);
+  );
 
 # Set up menus
 
-$mw->configure( -menu => my $menubar = $mw->Menu );
-map {$menubar->cascade( -tearoff => 0, -label => $_->[0], -menuitems => $_->[1] ) }
-['File', file_menuitems()],
-['Edit', edit_menuitems()],
-['Search', search_menuitems()],
-['Bookmarks', bookmark_menuitems()],
-['Selection', selection_menuitems()],
-['Fixup', fixup_menuitems()],
-['Text Processing', text_menuitems()],
-['HTML', html_menuitems()],
-['External', external_menuitems()],
-['Unicode', unicode_menuitems()],
-['Prefs', prefs_menuitems()],
-['Help', help_menuitems()];
-
+$mw->configure( -menu => my $menubar =
+      $mw->Menu( -menuitems => menubar_menuitems() ) );
 
 die "ERROR: too many files specified. \n" if ( @ARGV > 1 );
 
 if (@ARGV) {
     $globals{global_filename} = shift @ARGV;
     if ( -e $globals{global_filename} ) {
-        $mw->update
-        ;    # it may be a big file, draw the window, and then load it
+        $mw->update;   # it may be a big file, draw the window, and then load it
         openfile( $globals{global_filename} );
     }
-} else {
+}
+else {
     $globals{global_filename} = 'No File Loaded';
 }
 
-
-
-$textwindow->focus; 
+$textwindow->focus;
 MainLoop;
 
 ## Functions and subroutines
 
 # Menu functions
-sub file_menuitems {
+sub menubar_menuitems {
     [
-        ['command', 'Open', -command => \&file_open, -underline => 0,
-        -accelerator => 'Ctrl+o'],
-        '',
-        ['command', 'Save', -command => \&file_save, -underline => 0,
-        -accelerator => 'Ctrl+s'],
-        ['command', 'Save As', -command => \&file_saveas, -underline => 5, ],
-        '',
-        ['command', 'Exit', -command => \&myexit, -underline => 1, -accelerator => 'Ctrl+q' ],
+        map [ 'cascade', $_->[0], -menuitems => $_->[1] ],
+        [ 'File',            file_menuitems() ],
+        [ 'Edit',            edit_menuitems() ],
+        [ 'Search',          search_menuitems() ],
+        [ 'Bookmarks',       bookmark_menuitems() ],
+        [ 'Selection',       selection_menuitems() ],
+        [ 'Fixup',           fixup_menuitems() ],
+        [ 'Text Processing', text_menuitems() ],
+        [ 'HTML',            html_menuitems() ],
+        [ 'External',        external_menuitems() ],
+        [ 'Unicode',         unicode_menuitems() ],
+        [ 'Prefs',           prefs_menuitems() ],
+        [ 'Help',            help_menuitems() ],
     ];
 }
 
 # Base menu items
-sub edit_menuitems { }
-sub search_menuitems { }
-sub bookmark_menuitems { }
+sub file_menuitems {
+    [
+        [
+            'command', 'Open',
+            -command     => \&file_open,
+            -underline   => 0,
+            -accelerator => 'Ctrl+o'
+        ],
+        '',
+        [
+            'command', 'Save',
+            -command     => \&file_save,
+            -underline   => 0,
+            -accelerator => 'Ctrl+s'
+        ],
+        [ 'command', 'Save As', -command => \&file_saveas, -underline => 5, ],
+        [ 'command', 'Include', -command => \&file_include, -underline => 0, ],
+        [ 'command', 'Close', -command => \&file_close, -underline => 0, ],
+        '',
+        [ 'command', 'Import Prep Text Files', -command => \&prep_import, ],
+        [ 'command', 'Export As Prep Text Files', -command => \&prep_export, ],
+        '',
+        [ 'command', 'Guess Page Markers', -command => \&guess_pagemarks,
+        -underline => 0,],
+        [ 'command', 'Set Page Markers', -command => \&set_pagemarks, -underline
+        => 9, ],
+        '',
+        [
+            'command', 'Exit',
+            -command     => \&myexit,
+            -underline   => 1,
+            -accelerator => 'Ctrl+q'
+        ],
+    ];
+}
+
+sub edit_menuitems      {
+    [
+        [ 'command', 'Undo', ],
+        [ 'command', 'Redo', ],
+        '',
+        [ 'command', 'Cut', ],
+        [ 'command', 'Copy', ],
+        [ 'command', 'Paste', ],
+        [ 'command', 'Column Paste', ],
+        '',
+        [ 'command', 'Select All', ],
+        [ 'command', 'Unselect All', ],
+    ];
+}
+    
+sub search_menuitems    { }
+sub bookmark_menuitems  { }
 sub selection_menuitems { }
-sub fixup_menuitems { }
-sub text_menuitems { }
-sub html_menuitems { }
-sub external_menuitems { }
-sub unicode_menuitems { }
-sub prefs_menuitems { }
-sub help_menuitems { }
+sub fixup_menuitems     { }
+sub text_menuitems      { }
+sub html_menuitems      { }
+sub external_menuitems  { }
+sub unicode_menuitems   { }
+sub prefs_menuitems     { }
+sub help_menuitems      { }
 
 # File functions
-sub file_open { }
-sub file_save { }
+sub file_open   { }
+sub file_save   { }
 sub file_saveas { }
+sub file_include { }
+sub file_close { }
+
+# Guiprep texts processing 
+sub prep_import { }
+sub prep_export { }
+
+# Page Markers
+sub guess_pagemarks { }
+sub set_pagemarks { }
 
 # Exit functions
-sub myexit { exit } # This is really Tk::exit
+sub myexit         { exit }    # This is really Tk::exit
 sub confirmdiscard { }
+
