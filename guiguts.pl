c7d3d3a2c92ae7251e9000c088f4f7e3799b43be
vlsimpson <vlsimpson@6abef783-aa3e-0410-acc4-8c2641982bcc>
2009-12-02 06:40:21 +0000

    Minor formatting.

--- guiguts.pl
+++ guiguts.pl
@@ -282,8 +282,9 @@ my $menu = $top->Menu( -type => 'menubar' );
 
 $top->configure( -menu => $menu );
 
+# routines to call every time the text is edited
 $textwindow->SetGUICallbacks(
-    [    # routines to call every time the text is edited
+    [    
         \&update_indicators,
         sub {
             return if $nohighlights;
@@ -295,14 +296,16 @@ $textwindow->SetGUICallbacks(
     ]
 );
 
-buildmenu();    # Set up the custom menus
+# Set up the custom menus
+buildmenu();    
 
-textbindings(); # Set up the key bindings for the text widget
+# Set up the key bindings for the text widget
+textbindings(); 
 
-buildstatusbar();    # Build the status bar
+buildstatusbar();
 
-$top->Icon( -image => $icon )
-    ;  # Load the icon ito the window bar. Needs to happen late in the process
+# Load the icon ito the window bar. Needs to happen late in the process
+$top->Icon( -image => $icon );  
 
 $textwindow->focus;
 
@@ -332,8 +335,6 @@ set_autosave() if $autosave;
 $textwindow->CallNextGUICallback;
 
 $top->repeat( 200, \&updatesel );
-
-MainLoop;
 
 ############################################################################################
 
@@ -20813,3 +20814,5 @@ sub html_convert_tb {
     }
 
 }
+
+MainLoop;
