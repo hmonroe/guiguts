5931c7a5ae8fa0ce44a98e20fbb5e2ed195d2d77
vlsimpson <vlsimpson@6abef783-aa3e-0410-acc4-8c2641982bcc>
2009-12-04 03:37:41 +0000

    GNU reference in about_pop_up.

--- guiguts.pl
+++ guiguts.pl
@@ -18217,10 +18217,19 @@ other useful postprocessing functions.
 This version produced by a number of volunteers.
 See the Thanks.txt file for details.
 
-This program may be freely used, modified and distributed.
-No guarantees are made as to its fitness for any purpose.
-Any damages or losses resulting from the use of this software
-are the responsibility of the user.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 Original guiguts written by Stephen Schulze.
 Partially based on the Gedi editor - Gregs editor.
