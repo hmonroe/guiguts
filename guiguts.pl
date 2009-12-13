0b2f7e31d0d4d7e2170e71f6bbc0c84c86bb691c
vlsimpson <vlsimpson@6abef783-aa3e-0410-acc4-8c2641982bcc>
2009-12-13 19:43:21 +0000

    Clean up regexps.

--- guiguts.pl
+++ guiguts.pl
@@ -14065,19 +14065,19 @@ sub brackets {
         while (1) {
             last
                 unless (
-                (      ( $lglobal{brbrackets}[0] =~ m{[\[\(\{\<«]} )
-                    && ( $lglobal{brbrackets}[1] =~ m{[\]\)\}\>»]} )
+                (      ( $lglobal{brbrackets}[0] =~ m{[\[\(\{<«]} )
+                    && ( $lglobal{brbrackets}[1] =~ m{[\]\)\}>»]} )
                 )
-                || (   ( $lglobal{brbrackets}[0] =~ m{[\[\(\{\<»]} )
-                    && ( $lglobal{brbrackets}[1] =~ m{[\]\)\}\>«]} ) )
+                || (   ( $lglobal{brbrackets}[0] =~ m{[\[\(\{<»]} )
+                    && ( $lglobal{brbrackets}[1] =~ m{[\]\)\}>«]} ) )
                 || (   ( $lglobal{brbrackets}[0] =~ m{^\x7f*/\*} )
                     && ( $lglobal{brbrackets}[1] =~ m{^\x7f*\*/} ) )
                 || (   ( $lglobal{brbrackets}[0] =~ m{^\x7f*/\$} )
                     && ( $lglobal{brbrackets}[1] =~ m{^\x7f*\$/} ) )
                 || (   ( $lglobal{brbrackets}[0] =~ m{^\x7f*/[p]}i )
                     && ( $lglobal{brbrackets}[1] =~ m{^\x7f*[p]/}i ) )
-                || (   ( $lglobal{brbrackets}[0] =~ m{^\x7f*/\#} )
-                    && ( $lglobal{brbrackets}[1] =~ m{^\x7f*\#/} ) )
+                || (   ( $lglobal{brbrackets}[0] =~ m{^\x7f*/#} )
+                    && ( $lglobal{brbrackets}[1] =~ m{^\x7f*#/} ) )
                 );
             shift @{ $lglobal{brbrackets} };
             shift @{ $lglobal{brbrackets} };
