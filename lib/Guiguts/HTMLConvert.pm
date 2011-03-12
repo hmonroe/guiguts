24212572cbf79b7f25e4c09a7e895d0f80833dbe
hmonroe <hmonroe@6abef783-aa3e-0410-acc4-8c2641982bcc>
2011-03-12 21:32:11 +0000

    Move html_convert_body to HTMLConvert.pm

--- lib/Guiguts/HTMLConvert.pm
+++ lib/Guiguts/HTMLConvert.pm
@@ -7,7 +7,7 @@ BEGIN {
 	@ISA=qw(Exporter);
 	@EXPORT=qw(&html_convert_tb &htmlbackup &html_convert_subscripts &html_convert_superscripts
 	&html_convert_ampersands &html_convert_emdashes &html_convert_latin1 &html_convert_codepage &html_convert_utf
-	&html_cleanup_markers &html_convert_footnotes)
+	&html_cleanup_markers &html_convert_footnotes &html_convert_body)
 }
 
 sub html_convert_tb {
@@ -247,6 +247,513 @@ sub html_convert_footnotes {
 	}
 	
 }
+
+sub html_convert_body {
+	my ($textwindow, @contents) = @_;
+	&main::working('Converting Body');
+	my $aname = q{};
+	my $author;
+	my $blkquot = 0;
+	my $cflag   = 0;
+	my $front;
+	my $headertext;
+	my $inblock    = 0;
+	my $incontents = '1.0';
+	my $indent     = 0;
+	my $intitle    = 0;
+	my $ital       = 0;
+	my $listmark   = 0;
+	my $pgoffset   = 0;
+	my $poetry     = 0;
+	my $selection  = q{};
+	my $skip       = 0;
+	my $thisblank  = q{};
+	my $thisblockend;
+	my $thisblockstart = '1.0';
+	my $thisend        = q{};
+	my @last5        = [ '1', '1', '1', '1', '1', '1' ];
+	my $step         = 1;
+	my ( $ler, $lec );
+	
+	$thisblockend = $textwindow->index('end');
+	my ( $blkopen, $blkclose );
+	if ( $lglobal{cssblockmarkup} ) {
+		$blkopen  = '<div class="blockquot"><p>';
+		$blkclose = '</p></div>';
+	} else {
+		$blkopen  = '<blockquote><p>';
+		$blkclose = '</p></blockquote>';
+	}
+	
+	
+	( $ler, $lec ) = split /\./, $thisblockend;
+	while ( $step <= $ler ) {
+		unless ( $step % 500 ) {
+			$textwindow->see("$step.0");
+			$textwindow->update;
+		}
+		$selection = $textwindow->get( "$step.0", "$step.end" );
+		$incontents = "$step.end"
+		  if (    ( $step < 100 )
+			   && ( $selection =~ /contents/i )
+			   && ( $incontents eq '1.0' ) );
+
+		# Subscripts
+		html_convert_subscripts($textwindow, $selection, $step );
+
+		# Superscripts
+		html_convert_superscripts($textwindow, $selection, $step );
+
+		# Thought break conversion
+		html_convert_tb( $textwindow,$selection, $step );
+
+	 # /x|/X gets <pre>
+		if ( $selection =~ m"^/x"i ) {
+			$skip = 1;
+			$textwindow->ntdelete( "$step.0", "$step.end" );
+			$textwindow->insert( "$step.0", '<pre>' );
+			if ( ( $last5[2] ) && ( !$last5[3] ) ) {
+				$textwindow->ntinsert( ( $step - 2 ) . ".end", '</p>' )
+				  unless (
+						   $textwindow->get( ( $step - 2 ) . '.0',
+											 ( $step - 2 ) . '.end' ) =~ /<\/p>/
+				  );
+			}
+			$step++;
+			next;
+		}
+
+		# and end tag gets </pre>
+		if ( $selection =~ m"^x/"i ) {
+			$skip = 0;
+			$textwindow->ntdelete( "$step.0", "$step.end" );
+			$textwindow->ntinsert( "$step.0", '</pre>' );
+			$step++;
+			$step++;
+			next;
+		}
+		if ($skip) {
+			$step++;
+			next;
+		}
+		if ( $selection =~ m"^/f"i ) {
+			$front = 1;
+			$textwindow->ntdelete( "$step.0", "$step.end +1c" );
+			if ( ( $last5[2] ) && ( !$last5[3] ) ) {
+				$textwindow->ntinsert( ( $step - 2 ) . ".end", '</p>' )
+				  unless (
+						   $textwindow->get( ( $step - 2 ) . '.0',
+											 ( $step - 2 ) . '.end' ) =~ /<\/p>/
+				  );
+			}
+			next;
+		}
+		if ($front) {
+			if ( $selection =~ m"^f/"i ) {
+				$front = 0;
+				$textwindow->ntdelete( "$step.0", "$step.end +1c" );
+				$step++;
+				next;
+			}
+			if ( $selection =~ /^<h/ ) {
+				push @last5, $selection;
+				shift @last5 while ( scalar(@last5) > 4 );
+				$step++;
+				next;
+			}
+			$textwindow->ntinsert( "$step.0", '<p class="center">' )
+			  if (    length($selection)
+				   && ( !$last5[3] )
+				   && ( $selection !~ /<\/?h\d?|<br.*?>|<\/p>|<\/div>/ ) );
+			$textwindow->ntinsert( ( $step - 1 ) . '.end', '</p>' )
+			  if (   !( length($selection) )
+				   && ( $last5[3] )
+				   && ( $last5[3] !~ /<\/?h\d?|<br.*?>|<\/p>|<\/div>/ ) );
+			push @last5, $selection;
+			shift @last5 while ( scalar(@last5) > 4 );
+			$step++;
+			next;
+		}
+		if ( $lglobal{poetrynumbers} && ( $selection =~ s/\s\s(\d+)$// ) ) {
+			$selection .= '<span class="linenum">' . $1 . '</span>';
+			$textwindow->ntdelete( "$step.0", "$step.end" );
+			$textwindow->ntinsert( "$step.0", $selection );
+		}
+		if ($poetry) {
+			if ( $selection =~ /^\x7f*[pP]\/<?/ ) {
+				$poetry    = 0;
+				$selection = '</div></div>';
+				$textwindow->ntdelete( "$step.0", "$step.0 +2c" );
+				$textwindow->ntinsert( "$step.0", $selection );
+				push @last5, $selection;
+				shift @last5 while ( scalar(@last5) > 4 );
+				$ital = 0;
+				$step++;
+				next;
+			}
+			if ( $selection =~ /^$/ ) {
+				$textwindow->ntinsert( "$step.0",
+									   '</div><div class="stanza">' );
+				while (1) {
+					$step++;
+					$selection = $textwindow->get( "$step.0", "$step.end" );
+					last if ( $step ge $ler );
+					next if ( $selection =~ /^$/ );
+					last;
+				}
+				next;
+			}
+			if ( $selection =~
+				 s/\s{2,}(\d+)\s*$/<span class="linenum">$1<\/span>/ )
+			{
+				$textwindow->ntdelete( "$step.0", "$step.end" );
+				$textwindow->ntinsert( "$step.0", $selection );
+			}
+			my $indent = 0;
+			$indent = length($1) if $selection =~ s/^(\s+)//;
+			$textwindow->ntdelete( "$step.0", "$step.$indent" ) if $indent;
+			$indent -= 4;
+			$indent = 0 if ( $indent < 0 );
+			my ( $op, $cl ) = ( 0, 0 );
+			while ( ( my $temp = index $selection, '<i>', $op ) > 0 ) {
+				$op = $temp + 3;
+			}
+			while ( ( my $temp = index $selection, '</i>', $cl ) > 0 ) {
+				$cl = $temp + 4;
+			}
+			if ( !$cl && $ital ) {
+				$textwindow->ntinsert( "$step.end", '</i>' );
+			}
+			if ( !$op && $ital ) {
+				$textwindow->ntinsert( "$step.0", '<i>' );
+			}
+			if ( $op && $cl && ( $cl < $op ) && $ital ) {
+				$textwindow->ntinsert( "$step.0",   '<i>' );
+				$textwindow->ntinsert( "$step.end", '</i>' );
+			}
+			if ( $op && ( $cl < $op ) && !$ital ) {
+				$textwindow->ntinsert( "$step.end", '</i>' );
+				$ital = 1;
+			}
+			if ( $cl && ( $op < $cl ) && $ital ) {
+				if ($op) {
+					$textwindow->ntinsert( "$step.0", '<i>' );
+				}
+				$ital = 0;
+			}
+			$lglobal{classhash}->{$indent} =
+			    '    .poem span.i' 
+			  . $indent
+			  . '     {display: block; margin-left: '
+			  . $indent
+			  . 'em; padding-left: 3em; text-indent: -3em;}' . "\n"
+			  if ( $indent and ( $indent != 2 ) and ( $indent != 4 ) );
+			$textwindow->ntinsert( "$step.0",   "<span class=\"i$indent\">" );
+			$textwindow->ntinsert( "$step.end", '<br /></span>' );
+			push @last5, $selection;
+			shift @last5 while ( scalar(@last5) > 4 );
+			$step++;
+			next;
+		}
+		if ( $selection =~ /^\x7f*\/[pP]$/ ) {
+			$poetry = 1;
+			if ( ( $last5[2] ) && ( !$last5[3] ) ) {
+				$textwindow->ntinsert( ( $step - 2 ) . ".end", '</p>' )
+				  unless (
+						   $textwindow->get( ( $step - 2 ) . '.0',
+											 ( $step - 2 ) . '.end' ) =~ /<\/p>/
+				  );
+			}
+			$textwindow->ntdelete( $step . '.end -2c', $step . '.end' );
+			$selection = '<div class="poem"><div class="stanza">';
+			$textwindow->ntinsert( $step . '.end', $selection );
+			push @last5, $selection;
+			shift @last5 while ( scalar(@last5) > 4 );
+			$step++;
+			next;
+		}
+		if ( $selection =~ /^\x7f*\/\#/ ) {
+			$blkquot = 1;
+			push @last5, $selection;
+			shift @last5 while ( scalar(@last5) > 4 );
+			$step++;
+			$selection = $textwindow->get( "$step.0", "$step.end" );
+			$selection =~ s/^\s+//;
+			$textwindow->ntdelete( "$step.0", "$step.end" );
+			$textwindow->ntinsert( "$step.0", $blkopen . $selection );
+
+			if ( ( $last5[1] ) && ( !$last5[2] ) ) {
+				$textwindow->ntinsert( ( $step - 3 ) . ".end", '</p>' )
+				  unless (
+						   $textwindow->get( ( $step - 3 ) . '.0',
+											 ( $step - 2 ) . '.end' ) =~
+						   /<\/?h\d?|<br.*?>|<\/p>|<\/div>/
+				  );
+			}
+			$textwindow->ntinsert( ($step) . ".end", '</p>' )
+			  unless (
+					   length $textwindow->get(
+									( $step + 1 ) . '.0', ( $step + 1 ) . '.end'
+					   )
+			  );
+			push @last5, $selection;
+			shift @last5 while ( scalar(@last5) > 4 );
+			$step++;
+			next;
+		}
+		if ( $selection =~ /^\x7f*\/[Ll]/ ) {
+			$listmark = 1;
+			if ( ( $last5[2] ) && ( !$last5[3] ) ) {
+				$textwindow->ntinsert( ( $step - 2 ) . ".end", '</p>' )
+				  unless (
+						   $textwindow->get( ( $step - 2 ) . '.0',
+											 ( $step - 2 ) . '.end' ) =~ /<\/p>/
+				  );
+			}
+			$textwindow->ntdelete( "$step.0", "$step.end" );
+			$step++;
+			$selection = $textwindow->get( "$step.0", "$step.end" );
+			$selection = '<ul><li>' . $selection . '</li>';
+			$textwindow->ntdelete( "$step.0", "$step.end" );
+			$textwindow->ntinsert( "$step.0", $selection );
+			push @last5, $selection;
+			shift @last5 while ( scalar(@last5) > 4 );
+			$step++;
+			next;
+		}
+		if ( $selection =~ /^\x7f*\#\// ) {
+			$blkquot = 0;
+			$textwindow->ntinsert( ( $step - 1 ) . '.end', $blkclose );
+			push @last5, $selection;
+			shift @last5 while ( scalar(@last5) > 4 );
+			$step++;
+			next;
+		}
+		if ( $selection =~ /^\x7f*[Ll]\// ) {
+			$listmark = 0;
+			$textwindow->ntdelete( "$step.0", "$step.end" );
+			$textwindow->ntinsert( "$step.end", '</ul>' );
+			push @last5, '</ul>';
+			shift @last5 while ( scalar(@last5) > 4 );
+			$step++;
+			next;
+		}
+		if ($listmark) {
+			if ( $selection eq '' ) { $step++; next; }
+			$textwindow->ntdelete( "$step.0", "$step.end" );
+			my ( $op, $cl ) = ( 0, 0 );
+			while ( ( my $temp = index $selection, '<i>', $op ) > 0 ) {
+				$op = $temp + 3;
+			}
+			while ( ( my $temp = index $selection, '</i>', $cl ) > 0 ) {
+				$cl = $temp + 4;
+			}
+			if ( !$cl && $ital ) {
+				$selection .= '</i>';
+			}
+			if ( !$op && $ital ) {
+				$selection = '<i>' . $selection;
+			}
+			if ( $op && $cl && ( $cl < $op ) && $ital ) {
+				$selection = '<i>' . $selection;
+				$selection .= '</i>';
+			}
+			if ( $op && ( $cl < $op ) && !$ital ) {
+				$selection .= '</i>';
+				$ital = 1;
+			}
+			if ( $cl && ( $op < $cl ) && $ital ) {
+				if ($op) {
+					$selection = '<i>' . $selection;
+				}
+				$ital = 0;
+			}
+			$textwindow->ntinsert( "$step.0", '<li>' . $selection . '</li>' );
+			push @last5, $selection;
+			shift @last5 while ( scalar(@last5) > 4 );
+			$step++;
+			next;
+		}
+		if ($blkquot) {
+			if ( $selection =~ s/^(\s+)// ) {
+				my $space = length $1;
+				$textwindow->ntdelete( "$step.0", "$step.0 +${space}c" );
+			}
+		}
+		if ( $selection =~ /^\x7f*[\$\*]\// ) {
+			$inblock = 0;
+			$ital    = 0;
+			$textwindow->replacewith( "$step.0", "$step.end", '</p>' );
+			$step++;
+			next;
+		}
+		if ( $selection =~ /^\x7f*\/[\$\*]/ ) {
+			$inblock = 1;
+			if ( ( $last5[2] ) && ( !$last5[3] ) ) {
+				$textwindow->ntinsert( ( $step - 2 ) . '.end', '</p>' )
+				  unless (
+						   (
+							 $textwindow->get( ( $step - 2 ) . '.0',
+											   ( $step - 2 ) . '.end' ) =~
+							 /<\/?[hd]\d?|<br.*?>|<\/p>/
+						   )
+				  );
+			}
+			$textwindow->replacewith( "$step.0", "$step.end", '<p>' );
+			$step++;
+			next;
+		}
+		if ( ( $last5[2] ) && ( !$last5[3] ) ) {
+			$textwindow->ntinsert( ( $step - 2 ) . '.end', '</p>' )
+			  unless (
+					   (
+						 $textwindow->get( ( $step - 2 ) . '.0',
+										   ( $step - 2 ) . '.end' ) =~
+						 /<\/?[hd]\d?|<br.*?>|<\/p>|<\/[uo]l>/
+					   )
+					   || ($inblock)
+			  );
+		}
+		if ( $inblock || ( $selection =~ /^\s/ ) ) {
+			if ( $last5[3] ) {
+				if ( $last5[3] =~ /^\S/ ) {
+					$last5[3] .= '<br />';
+					$textwindow->ntdelete( ( $step - 1 ) . '.0',
+										   ( $step - 1 ) . '.end' );
+					$textwindow->ntinsert( ( $step - 1 ) . '.0', $last5[3] );
+				}
+			}
+			$thisend = $textwindow->index( $step . ".end" );
+			$textwindow->ntinsert( $thisend, '<br />' );
+			if ( $selection =~ /^(\s+)/ ) {
+				$indent = ( length($1) / 2 );
+				$selection =~ s/^\s+//;
+				$selection =~ s/  /&nbsp; /g;
+				$selection =~ s/(&nbsp; ){1,}\s?(<span class="linenum">)/ $2/g;
+				my ( $op, $cl ) = ( 0, 0 );
+				while ( ( my $temp = index $selection, '<i>', $op ) > 0 ) {
+					$op = $temp + 3;
+				}
+				while ( ( my $temp = index $selection, '</i>', $cl ) > 0 ) {
+					$cl = $temp + 4;
+				}
+				if ( !$cl && $ital ) {
+					$selection .= '</i>';
+				}
+				if ( !$op && $ital ) {
+					$selection = '<i>' . $selection;
+				}
+				if ( $op && $cl && ( $cl < $op ) && $ital ) {
+					$selection = '<i>' . $selection;
+					$selection .= '</i>';
+				}
+				if ( $op && ( $cl < $op ) && !$ital ) {
+					$selection .= '</i>';
+					$ital = 1;
+				}
+				if ( $cl && ( $op < $cl ) && $ital ) {
+					if ($op) {
+						$selection = '<i>' . $selection;
+					}
+					$ital = 0;
+				}
+				$selection =
+				    '<span style="margin-left: ' 
+				  . $indent . 'em;">'
+				  . $selection
+				  . '</span>';
+				$textwindow->ntdelete( "$step.0", $thisend );
+				$textwindow->ntinsert( "$step.0", $selection );
+			}
+			if ( ( $last5[2] ) && ( !$last5[3] ) && ( $selection =~ /\/\*/ ) ) {
+				$textwindow->ntinsert( ( $step - 2 ) . ".end", '</p>' )
+				  unless (
+						   $textwindow->get( ( $step - 2 ) . '.0',
+										( $step - 2 ) . '.end' ) =~ /<\/[hd]\d?/
+				  );
+			}
+			push @last5, $selection;
+			shift @last5 while ( scalar(@last5) > 4 );
+			$step++;
+			next;
+		}
+		{
+
+			no warnings qw/uninitialized/;
+			if (    ( !$last5[0] )
+				 && ( !$last5[1] )
+				 && ( !$last5[2] )
+				 && ( !$last5[3] )
+				 && ($selection) )
+			{
+				$textwindow->ntinsert( ( $step - 1 ) . '.0',
+									   '<hr style="width: 65%;" />' )
+				  unless ( $selection =~ /<[ph]/ );
+				$aname =~ s/<\/?[hscalup].*?>//g;
+				$aname = makeanchor( deaccent($selection) );
+				$textwindow->ntinsert(
+									   "$step.0",
+									   "<h2><a name=\"" 
+										 . $aname
+										 . "\" id=\""
+										 . $aname
+										 . "\"></a>"
+				) unless ( $selection =~ /<[ph]/ );
+				$textwindow->ntinsert( "$step.end", '</h2>' )
+				  unless ( $selection =~ /<[ph]/ );
+				unless ( $selection =~ /<p/ ) {
+					$selection =~ s/<sup>.*?<\/sup>//g;
+					$selection =~ s/<[^>]+>//g;
+					$selection = "<b>$selection</b>";
+					push @contents,
+					    "<a href=\"#" 
+					  . $aname . "\">"
+					  . $selection
+					  . "</a><br />\n";
+				}
+				$selection .= '<h2>';
+				$textwindow->see("$step.0");
+				$textwindow->update;
+			} elsif ( ( $last5[2] =~ /<h2>/ ) && ($selection) ) {
+				$textwindow->ntinsert( "$step.0", '<p>' )
+				  unless (    ( $selection =~ /<[pd]/ )
+						   || ( $selection =~ /<[hb]r>/ )
+						   || ($inblock) );
+			} elsif ( ( $last5[2] ) && ( !$last5[3] ) && ($selection) ) {
+				$textwindow->ntinsert( "$step.0", '<p>' )
+				  unless (    ( $selection =~ /<[phd]/ )
+						   || ( $selection =~ /<[hb]r>/ )
+						   || ($inblock) );
+			} elsif (    ( $last5[1] )
+					  && ( !$last5[2] )
+					  && ( !$last5[3] )
+					  && ($selection) )
+			{
+				$textwindow->ntinsert( "$step.0", '<p>' )
+				  unless (    ( $selection =~ /<[phd]/ )
+						   || ( $selection =~ /<[hb]r>/ )
+						   || ($inblock) );
+			} elsif (    ( $last5[0] )
+					  && ( !$last5[1] )
+					  && ( !$last5[2] )
+					  && ( !$last5[3] )
+					  && ($selection) )
+			{
+				$textwindow->ntinsert( "$step.0", '<p>' )
+				  unless (    ( $selection =~ /<[phd]/ )
+						   || ( $selection =~ /<[hb]r>/ )
+						   || ($inblock) );
+			}
+		}
+		push @last5, $selection;
+		shift @last5 while ( scalar(@last5) > 4 );
+		$step++;
+	}
+	push @contents, '</p>';
+
+	
+}
+
 
 1;
 
