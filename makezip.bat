d737c30390f09417b255ff8aff5f834ee6b95c36
hmonroe <hmonroe@6abef783-aa3e-0410-acc4-8c2641982bcc>
2011-09-30 03:51:24 +0000

    Exclude make files from zip

--- makezip.bat
+++ makezip.bat
@@ -1,3 +1,3 @@
 erase *.zip
-7z a -x!.* -x!setting.rc -x!header.txt -r WinGuts-0.3.10.zip *.*
-7z a -x!.* -x!setting.rc -x!header.txt -x!WinGuts.exe -x!*.zip -r guiguts-0.3.10.zip *.*
+7z a -x!.* -x!setting.rc -x!header.txt -x!make*.* -r WinGuts-0.3.10.zip *.*
+7z a -x!.* -x!setting.rc -x!header.txt -x!WinGuts.exe -x!*.zip -x!make*.* -r guiguts-0.3.10.zip *.*
