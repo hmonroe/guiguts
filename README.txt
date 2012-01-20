bb10939a9090c169994d9777a48d0278b566cfd4
hmonroe <hmonroe@6abef783-aa3e-0410-acc4-8c2641982bcc>
2012-01-20 05:26:26 +0000

    Guiguts.bat calls perl in a way that should ignore preexisting installations of perl.

--- README.txt
+++ README.txt
@@ -16,7 +16,8 @@ files as input. PPV TXT and PP HTML labeled more accurately as pptxt and
 pphtml. Only README.TXT appears in the prepopulated recently used file
 list. Search can find the first word in the file. Word frequency rerun
 after typing words in empty file reports now works and bug with
-unresponsive save as dialog fixed.
+unresponsive save as dialog fixed. Guiguts.bat calls perl in a way
+that should ignore preexisting installations of perl.
 
 Version 1.0.3. Relocated HTML page number outside an open <span> eg for a line
 of poetry so page numbers align vertically. Auto List on HTML palette no
--- guiguts.bat
+++ guiguts.bat
@@ -1,10 +1,10 @@
 echo off
 PATH=%PATH%;%cd%\perl;%cd%\perl\lib;%cd%\python27;%cd%\python27\scripts;%cd%\tools\groff\bin;%cd%\tools\kindlegen;%cd%\tools\tidy
-    set ENCFONTS=c:/dp/gnutenbergproj/gnutenberg/0.4/pdf/fonts/generated 
-    set TEXFONTS=c:/dp/gnutenbergproj/gnutenberg/0.4/pdf/fonts/generated
-    set TEXFONTMAPS=c:/dp/gnutenbergproj/gnutenberg/0.4/pdf/fonts/generated
-    set TFMFONTS=c:/dp/gnutenbergproj/gnutenberg/0.4/pdf/fonts/generated
-    set TTFONTS=c:/dp/gnutenbergproj/gnutenberg/0.4/pdf/fonts/generated
-    set TEXPSHEADERS=c:/dp/gnutenbergproj/gnutenberg/0.4/pdf/fonts/generated
+    set ENCFONTS=c:/guiguts/tools/gnutenbergproj/gnutenberg/0.4/pdf/fonts/generated 
+    set TEXFONTS=c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated
+    set TEXFONTMAPS=c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated
+    set TFMFONTS=c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated
+    set TTFONTS=c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated
+    set TEXPSHEADERS=c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated
 
-perl guiguts.pl %1
+%cd%\perl\perl.exe guiguts.pl %1
