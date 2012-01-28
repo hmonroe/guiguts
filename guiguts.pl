3bd6c32473b194e2968214db11710c9914ee7224
roryoconor <roryoconor@6abef783-aa3e-0410-acc4-8c2641982bcc>
2012-01-28 00:58:39 +0000

    test commit again

--- guiguts.pl
+++ guiguts.pl
@@ -189,6 +189,7 @@ our $stayontop        = 0;
 our $suspectindex;
 our $toolside            = 'bottom';
 our $useppwizardmenus    = 0;
+our $usemenutwo          = 0;
 our $utffontname         = 'Courier New';
 our $utffontsize         = 14;
 our $verboseerrorchecks  = 0;
