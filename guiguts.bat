340b5971e2b1436e91272c2dd2fc24d856f205f0
hmonroe <hmonroe@6abef783-aa3e-0410-acc4-8c2641982bcc>
2012-01-29 04:45:51 +0000

    Fixed path in guiguts.bat for ENCFONTS used by the Gnutenberg Press.

--- guiguts.bat
+++ guiguts.bat
@@ -1,10 +1,10 @@
 echo off
 PATH=%cd%\perl;%cd%\perl\lib;%cd%\python27;%cd%\python27\scripts;%cd%\tools\groff\bin;%cd%\tools\kindlegen;%cd%\tools\tidy;%PATH%
-    set ENCFONTS=c:/guiguts/tools/gnutenbergproj/gnutenberg/0.4/pdf/fonts/generated 
-    set TEXFONTS=c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated
-    set TEXFONTMAPS=c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated
-    set TFMFONTS=c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated
-    set TTFONTS=c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated
+    set ENCFONTS=    c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated 
+    set TEXFONTS=    c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated
+    set TEXFONTMAPS= c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated
+    set TFMFONTS=    c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated
+    set TTFONTS=     c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated
     set TEXPSHEADERS=c:/guiguts/tools/gnutenberg/0.4/pdf/fonts/generated
 
 %cd%\perl\perl.exe guiguts.pl %1
