af9b8b98378a7787f132148cb7ace28ad4c4fcbe
hmonroe <hmonroe@6abef783-aa3e-0410-acc4-8c2641982bcc>
2012-02-20 22:13:53 +0000

    Put back working guiguts.pl

--- guiguts.pl
+++ guiguts.pl
@@ -14,7 +14,7 @@
 #Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 use strict;
 use warnings;
-
+ 
 #use criticism 'gentle';
 our $VERSION = '1.0.7';
 
@@ -59,6 +59,7 @@ use Tk::widgets qw{Balloon
 our $APP_NAME     = 'Guiguts';
 our $window_title = $APP_NAME . '-' . $VERSION;
 our $icondata;
+setup();
 ### Custom Guiguts modules
 use Guiguts::ASCIITables;
 use Guiguts::ErrorCheck;
@@ -74,7 +75,6 @@ use Guiguts::MenuStructure;
 use Guiguts::MultiLingual;
 use Guiguts::PageNumbers;
 use Guiguts::PageSeparators;
-use Guiguts::Preferences;
 use Guiguts::SearchReplaceMenu;
 use Guiguts::SelectionMenu;
 use Guiguts::SpellCheck;
@@ -85,6 +85,11 @@ use Guiguts::TextUnicode;
 use Guiguts::CharacterTools;
 use Guiguts::Utilities;
 use Guiguts::WordFrequency;
+
+# Ignore any watchdog timer alarms. Subroutines that take a long time to
+# complete can trip it
+$SIG{ALRM} = 'IGNORE';
+$SIG{INT} = sub { _exit() };
 ### Constants
 my $no_proofer_url  = 'http://www.pgdp.net/phpBB2/privmsg.php?mode=post';
 my $yes_proofer_url = 'http://www.pgdp.net/c/stats/members/mbr_list.php?uname=';
@@ -242,25 +247,118 @@ our @extops = (
 
 #All local global variables contained in one hash. # now global
 our %lglobal;    # need to document each variable
+
+# $lglobal{hl_index} 	line number being scanned for highlighting
 if ( eval { require Text::LevenshteinXS } ) {
 	$lglobal{LevenshteinXS} = 1;
 }
+
+#else {
+#	print
+#"Install the module Text::LevenshteinXS for much faster harmonics sorting.\n";
+#}
+# load Image::Size if it is installed
 if ( eval { require Image::Size; 1; } ) {
 	$lglobal{ImageSize} = 1;
 } else {
 	$lglobal{ImageSize} = 0;
 }
-our $top;
-our $icon;
-our $text_frame;
-our $counter_frame;
-our $proofer_frame;
-our $text_font;
-our $textwindow;
-our $menubar;
+
+# FIXME: Change $top to $mw.
+our $top = tkinit( -title => $window_title, );
 initialize();    # Initialize a bunch of vars that need it.
+$top->minsize( 440, 90 );
 
+# Detect geometry changes for tracking
+$top->bind(
+	'<Configure>' => sub {
+		$geometry = $top->geometry;
+		$lglobal{geometryupdate} = 1;
+	}
+);
+our $icon = $top->Photo( -format => 'gif',
+						 -data   => $icondata );
+fontinit();      # Initialize the fonts for the two windows
+utffontinit();
 
+# Set up Main window size
+unless ($geometry) {
+	my $height = $top->screenheight() - 90;
+	my $width  = $top->screenwidth() - 20;
+	$geometry = $width . "x" . $height . "+0+0";
+	$geometry = $top->geometry($geometry);
+}
+$top->geometry($geometry) if $geometry;
+
+# Set up Main window layout
+my $text_frame = $top->Frame->pack(
+									-anchor => 'nw',
+									-expand => 'yes',
+									-fill   => 'both'
+);
+our $counter_frame =
+  $text_frame->Frame->pack(
+							-side   => 'bottom',
+							-anchor => 'sw',
+							-pady   => 2,
+							-expand => 0
+  );
+
+# Frame to hold proofer names. Pack it when necessary.
+my $proofer_frame = $text_frame->Frame;
+our $text_font = $top->fontCreate(
+								   'courier',
+								   -family => "Courier New",
+								   -size   => 12,
+								   -weight => 'normal',
+);
+
+# The actual text widget
+our $textwindow = $text_frame->LineNumberText(
+	-widget          => 'TextUnicode',
+	-exportselection => 'true',        # 'sel' tag is associated with selections
+	-background      => $bkgcolor,
+	-relief          => 'sunken',
+	-font            => $lglobal{font},
+	-wrap            => 'none',
+	-curlinebg       => $::activecolor,
+  )->pack(
+		   -side   => 'bottom',
+		   -anchor => 'nw',
+		   -expand => 'yes',
+		   -fill   => 'both'
+  );
+$top->protocol( 'WM_DELETE_WINDOW' => \&_exit );
+$top->configure( -menu => our $menubar = $top->Menu );
+
+# routines to call every time the text is edited
+$textwindow->SetGUICallbacks(
+	[
+	   \&update_indicators,
+	   sub {
+		   return unless $nohighlights;
+		   $textwindow->HighlightAllPairsBracketingCursor;
+	   },
+	   sub {
+		   $textwindow->hidelinenum unless $vislnnm;
+		 }
+	]
+);
+
+# Set up the custom menus
+menubuild();
+
+# Set up the key bindings for the text widget
+textbindings();
+buildstatusbar( $textwindow, $top );
+
+# Load the icon into the window bar. Needs to happen late in the process
+$top->Icon( -image => $icon );
+$lglobal{hasfocus} = $textwindow;
+toolbar_toggle();
+$top->geometry($geometry) if $geometry;
+( $lglobal{global_filename} ) = @ARGV;
+die "ERROR: too many files specified. \n" if ( @ARGV > 1 );
 if (     ( $lglobal{global_filename} )
 	 and ( $lglobal{global_filename} eq 'runtests' ) )
 {
@@ -281,11 +379,181 @@ set_autosave() if $autosave;
 $textwindow->CallNextGUICallback;
 $top->repeat( 200, sub { _updatesel($textwindow) } );
 
+# Do not move from guiguts.pl; do command must be run in main
+sub loadscannos {
+	$lglobal{scannosfilename} = '';
+	%scannoslist = ();
+	@{ $lglobal{scannosarray} } = ();
+	$lglobal{scannosindex} = 0;
+	my $types = [ [ 'Scannos', ['.rc'] ], [ 'All Files', ['*'] ], ];
+	$scannospath = os_normal($scannospath);
+	$lglobal{scannosfilename} =
+	  $top->getOpenFile(
+						 -filetypes  => $types,
+						 -title      => 'Scannos list?',
+						 -initialdir => $scannospath
+	  );
+	if ( $lglobal{scannosfilename} ) {
+		my ( $name, $path, $extension ) =
+		  fileparse( $lglobal{scannosfilename}, '\.[^\.]*$' );
+		$scannospath = $path;
+		unless ( my $return = do $lglobal{scannosfilename} )
+		{    # load scannos list
+			unless ( defined $return ) {
+				if ($@) {
+					$top->messageBox(
+						-icon => 'error',
+						-message =>
+'Could not parse scannos file, file may be corrupted.',
+						-title => 'Problem with file',
+						-type  => 'Ok',
+					);
+				} else {
+					$top->messageBox(
+									 -icon    => 'error',
+									 -message => 'Could not find scannos file.',
+									 -title   => 'Problem with file',
+									 -type    => 'Ok',
+					);
+				}
+				$lglobal{doscannos} = 0;
+				return 0;
+			}
+		}
+		foreach ( sort ( keys %scannoslist ) ) {
+			push @{ $lglobal{scannosarray} }, $_;
+		}
+		if ( $lglobal{scannosfilename} =~ /reg/i ) {
+			searchoptset(qw/0 x x 1/);
+		} else {
+			searchoptset(qw/x x x 0/);
+		}
+		return 1;
+	}
+}
+## Save setting.rc file
+sub savesettings {
+
+	#print time()."savesettings\n";
+	my $message = <<EOM;
+# This file contains your saved settings for guiguts.
+# It is automatically generated when you save your settings.
+# If you delete it, all the settings will revert to defaults.
+# You shouldn't ever have to edit this file manually.\n\n
+EOM
+	my ( $index, $savethis );
+	my $thispath = $0;
+	$thispath =~ s/[^\\]*$//;
+	my $savefile = $thispath . 'setting.rc';
+	$geometry = $top->geometry;    # unless $geometry;
+	if ( open my $save_handle, '>', $savefile ) {
+		print $save_handle $message;
+		print $save_handle '@gcopt = (';
+		print $save_handle "$_," || '0,' for @gcopt;
+		print $save_handle ");\n\n";
+
+		# a variable's value is not saved unless it is nonzero
+		for (
+			qw/alpha_sort activecolor auto_page_marks auto_show_images autobackup autosave autosaveinterval bkgcolor
+			blocklmargin blockrmargin bold_char defaultindent failedsearch fontname fontsize fontweight geometry
+			geometry2 geometry3 geometrypnumpop globalaspellmode highlightcolor history_size ignoreversionnumber
+			intelligentWF ignoreversions italic_char jeebiesmode lastversioncheck lmargin multiterm nobell nohighlights
+			notoolbar poetrylmargin rmargin rwhyphenspace scannos_highlighted stayontop toolside utffontname utffontsize
+			useppwizardmenus usemenutwo verboseerrorchecks vislnnm w3cremote wfstayontop/
+		  )
+		{
+			if ( eval '$' . $_ ) {
+				print $save_handle "\$$_", ' ' x ( 20 - length $_ ), "= '",
+				  eval '$' . $_, "';\n";
+			}
+		}
+		print $save_handle "\n";
+		for (
+			qw/globallastpath globalspellpath globalspelldictopt globalviewerpath globalbrowserstart
+			gutcommand jeebiescommand scannospath tidycommand validatecommand validatecsscommand gnutenbergdirectory/
+		  )
+		{
+			if ( eval '$' . $_ ) {
+				print $save_handle "\$$_", ' ' x ( 20 - length $_ ), "= '",
+				  escape_problems( os_normal( eval '$' . $_ ) ), "';\n";
+			}
+		}
+		print $save_handle ("\n\@recentfile = (\n");
+		for (@recentfile) {
+			print $save_handle "\t'", escape_problems($_), "',\n";
+		}
+		print $save_handle (");\n\n");
+		print $save_handle ("\@extops = (\n");
+		for my $index ( 0 .. $#extops ) {
+			my $label   = escape_problems( $extops[$index]{label} );
+			my $command = escape_problems( $extops[$index]{command} );
+			print $save_handle
+			  "\t{'label' => '$label', 'command' => '$command'},\n";
+		}
+		print $save_handle ");\n\n";
+
+		#print $save_handle ("\%geometryhash = (\n");
+		for ( keys %geometryhash ) {
+			print $save_handle "\$geometryhash{$_} = '$geometryhash{$_}';\n";
+		}
+		print $save_handle "\n";
+		print $save_handle '@mygcview = (';
+		for (@mygcview) { print $save_handle "$_," }
+		print $save_handle (");\n\n");
+		print $save_handle ("\@search_history = (\n");
+		my @array = @search_history;
+		for my $index (@array) {
+			$index =~ s/([^A-Za-z0-9 ])/'\x{'.(sprintf "%x", ord $1).'}'/eg;
+			print $save_handle qq/\t"$index",\n/;
+		}
+		print $save_handle ");\n\n";
+		print $save_handle ("\@replace_history = (\n");
+		@array = @replace_history;
+		for my $index (@array) {
+			$index =~ s/([^A-Za-z0-9 ])/'\x{'.(sprintf "%x", ord $1).'}'/eg;
+			print $save_handle qq/\t"$index",\n/;
+		}
+		print $save_handle ");\n\n";
+		print $save_handle ("\@multidicts = (\n");
+		for my $index (@multidicts) {
+			print $save_handle qq/\t"$index",\n/;
+		}
+		print $save_handle ");\n\n1;\n";
+	}
+}
+
+# Do not move from guiguts.pl; do command must be run in main
+sub readsettings {
+	if ( -e 'setting.rc' ) {
+		unless ( my $return = do 'setting.rc' ) {
+			open my $file, "<", "setting.rc"
+			  or warn "Could not open setting file\n";
+			my @file = <$file>;
+			close $file;
+			my $settings = '';
+			for (@file) {
+				$settings .= $_;
+			}
+			unless ( my $return = eval($settings) ) {
+				if ( -e 'setting.rc' ) {
+					open my $file, "<", "setting.rc"
+					  or warn "Could not open setting file\n";
+					my @file = <$file>;
+					close $file;
+					open $file, ">", "setting.err";
+					print $file @file;
+					close $file;
+					print length($file);
+				}
+			}
+		}
+	}
+}
+
 sub dofile {
 	my $filename = shift;
 	do $filename;
 }
-
 # Ready to enter main loop
 checkforupdatesmonthly();
 unless ( -e 'header.txt' ) {
